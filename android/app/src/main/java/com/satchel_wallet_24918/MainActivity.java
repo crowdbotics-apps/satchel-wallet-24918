5fe0de36cddd053e6e2630b0026577956b0d0eab
Crowdbotics <team@crowdbotics.com>
2021-03-08 23:38:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/satchel_wallet_24918/MainActivity.java
@@ -0,0 +1,15 @@
+package com.satchel_wallet_24918;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "satchel_wallet_24918";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://satchel-wallet-24918.botics.co",
+  defaultTimeout: 5000
+};
